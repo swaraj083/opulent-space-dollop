ec475d2c37ddca8924c3f3b52824df8c52da6005
swaraj kendre <157608089+swaraj083@users.noreply.github.com>
2024-09-20 10:50:23 +0530

    Update App.jsx
    this is my first commint N APP JSX =  " change is done                 "

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            CHANGE IS DONE
           </a>
         </p>
       </header>
